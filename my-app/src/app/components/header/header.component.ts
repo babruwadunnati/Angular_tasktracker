63720ce6fcdbbd1ee396dcadabf375a42a90c387
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2023-10-14 19:05:37 -0700

    Changes

--- my-app/src/app/components/header/header.component.ts
+++ my-app/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  //Variable title which is displayed in header.html file
+  title: string = 'Task Tracker';
+  showAddTask : boolean = false;
+  subscription: Subscription;
+  constructor(private uiService: UiService, private router: Router){
+    this.subscription = this.uiService.onToggle().subscribe(value => (this.showAddTask= value));
+
+  }
+
+
+  //Method which we are using to work when we click on the button
+  toggleAddTask(){
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string){
+    return this.router.url == route;
+  }
 
 }
